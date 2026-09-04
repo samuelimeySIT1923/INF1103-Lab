8676c5302a1cfd31a5f34987923c467b9ec0164b
samue <2601923@sit.singaporetech.edu.sg>
2026-09-04 17:03:12 +0800

    Interactive profile creator

--- hello.py
+++ hello.py
@@ -1,21 +1,10 @@
-print("===============================")
-print("Welcome here")
-print("My first post!")
-print("===============================")
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
+username = input("Enter your Username: ")
+age = input("Enter age: ")
+category = input("Enter Content Category: ")
 
+print("\nInstagram Profile")
+print("=====================")
 print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
-
-followers += 50
-print("Day 1:", followers)
-
-followers += 20
-print("Day 2:", followers)
-
-followers -= 10
-print("Day 3:", followers)
+print("Age:", age)
+print("Category:", category)
