e1b18e6112f4fa83b2ffde1126f2dd5c2ab6fbc9
samue <2601923@sit.singaporetech.edu.sg>
2026-09-04 17:42:50 +0800

    Add all code from each part

--- hello.py
+++ hello.py
@@ -1,4 +1,46 @@
+#1) First Post
+print("=======================")
+print("Welcome here")
+print("My first post!")
+print("======================")
 
+
+#2) Update Profile
+print("=======================")
+print("Welcome here")
+print("My first post!")
+print("======================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+
+#3) Follower Growth
+followers = 100
+
+followers += 50 
+print("Day 1:", followers)
+
+followers += 20 
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
+
+#4) Interactive profile creator
+username = input("Enter your Username: ")
+age = input("Enter age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("=====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+#5) Pt 5. More Next Week
 username = input("Enter your Username: ")
 age = int(input("Enter age: "))
 category = input("Enter Content Category: ")
