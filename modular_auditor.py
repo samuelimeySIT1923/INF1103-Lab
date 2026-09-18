3adb5d3b1a816313a2075d397d3af00b7c0e237e
samue <2601923@sit.singaporetech.edu.sg>
2026-09-18 11:55:23 +0800

    W3_Define Funcitons V1

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,28 +1,52 @@
+#1. Initialize the inventory to zero in the start
+inv = 0
+quit = False
+entrycount = 0
+errorcount = 0
 
-#Requirements: 
-#Create a copy of your auditor.py from last week and name it modular_auditor.py. 
-#This ensures you have a backup of your Week 2 solution. 
+#4a. get_valid_input(): Handles the prompt, handles input validation, and returns a valid integer or a "quit" signal. 
+def get_valid_input():
+    if inputQty.lower() == 'quit':  
+            quit = True
+    if not (inputQty.lstrip('-').isdigit()):
+        print("Error: pls enter a valid number or type 'quit' to exit.")
+        errorcount += 1
+    if int(inputQty) < 0:
+        print("Error: Negative numbers are not allowed. Pls input a positive number or type 'quit' to exit.")
+        errorcount += 1
+
+ #4b. process_delivery(current_total, new_value): Calculates the new total and returns it.        
+def process_delivery(current_total, new_value):
+    current_total += new_value
+    return current_total + new_value
+
+#4c. calculate_tax(amount): A new requirement! This function takes a delivery amount and returns the tax (10% of that specific delivery). 
+def calculate_tax(amount):
+    tax = delivery_amount * 0.10
+    return tax
+
+#4d. generate_report(total_units, failed_attempts): A dedicated function to print the final summary. 
+def generate_report(total_units, failed_attempts):
+    print("\n\033[1m", "Report Summary:", "\033[0m")
+    print("Total Units Processed:", total_units)
+    print("Number of Failed/Rejected Entries:", failed_attempts)
+
+
+#2. Run in a continuous loop asking user to enter a stock quantity, until the user types quit. (Think of which loop might be helpful here: "for,or,while")
+while quit == False:
+    inputQty = input("Enter stock quantity (or type 'quit' to exit): ")
+    get_valid_input()
+    continue
 
 
 
 
-#4. Reporting: When the user types quit, print the Total Deliveries Processed        
-#and the Number of Failed/Rejected Entries. 
-#Your program must now consist of specific functions: 
-#1. get_valid_input(): Handles the prompt, handles input validation, and 
-#returns a valid integer or a "quit" signal. 
-#2. process_delivery(current_total, new_value): Calculates the new total and 
-#returns it. 
-#3. calculate_tax(amount): A new requirement! This function takes a delivery 
-#amount and returns the tax (10% of that specific delivery). 
-#4. generate_report(total_units, failed_attempts): A dedicated function to print 
-#the final summary. 
+
 #Don't write code straight away. Map out your function signatures. A function 
 #signature defines the inputs and outputs. Example: def process_delivery(total, 
 #value): return new_total. Map your logic: 
 #o What goes into get_valid_input()? (Nothing). What comes out? 
 #(The number or a 'quit' status). 
-#SIT Internal 
 #o What goes into calculate_tax()? (The delivery value). What comes 
 #out? (The tax amount). 
 #o Then write the code
@@ -31,29 +55,18 @@
 
 
 
-#1. Initialize the inventory to zero in the start
-inv = 0
-quit = False
-entrycount = 0
-errorcount = 0
 
-#2. Run in a continuous loop asking user to enter a stock quantity, until the user types quit. (Think of which loop might be helpful here: "for,or,while")
-while quit == False:
-    inputQty = input("Enter stock quantity (or type 'quit' to exit): ")
-    if inputQty.lower() == 'quit':  
-            quit = True
-            break
-    if not (inputQty.lstrip('-').isdigit()):
-        print("Error: pls enter a valid number or type 'quit' to exit.")
-        errorcount += 1
-        continue
-    if int(inputQty) < 0:
-        print("Error: Negative numbers are not allowed. Pls input a positive number or type 'quit' to exit.")
-        errorcount += 1
-        continue
+
+
+#3. If the user enters a valid value:  
+#• Add the delivery amount to the running total. 
+#• Calculate the tax for that delivery. 
+#• Update any counters and records you are tracking, such as the number of deliveries processed. 
+
+
     elif inputQty.isdigit():
         print("Stock Quantity entered:", inputQty)
-        
+
         inv += int(inputQty)
         if int(inv) > 500:
             print("ALERT: Overstock! Total inventory exceeds 500 units.")
@@ -61,13 +74,17 @@ while quit == False:
             break
         entrycount += 1
         print(entrycount,". Total Inventory:", inv)
-#3. If the user enters a valid value:  
-#• Add the delivery amount to the running total. 
-#• Calculate the tax for that delivery. 
-#• Update any counters and records you are tracking, such as the number 
-#of deliveries processed. 
+inputQty.isdigit():
+        print("Stock Quantity entered:", inputQty)
 
-print("\n\033[1m", "Report Summary:", "\033[0m")
-print("Total Units Processed:", inv)
-print("Number of Failed/Rejected Entries:", errorcount)
+        inv += int(inputQty)
+        if int(inv) > 500:
+            print("ALERT: Overstock! Total inventory exceeds 500 units.")
+            errorcount += 1
+            break
+        entrycount += 1
+        print(entrycount,". Total Inventory:", inv)
+return int(inputQty)
+
+
     
