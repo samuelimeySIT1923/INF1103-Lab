31934f9b10d366250c5ddaa763099d7cd959aad5
samue <2601923@sit.singaporetech.edu.sg>
2026-09-25 11:47:30 +0800

    Lab4 Debugging 1

--- persistent_auditor.py
+++ persistent_auditor.py
@@ -1,88 +1,101 @@
+#Initialize the inventory to zero in the start
+import re
 
 
-
-#4. Modularity: Maintain your functional design. Create a load_inventory() and save_inventory() function. 
-def load_inventory(totalorderlist):
-    #Persistence: At the start of the program, read the information previously saved in the inventory file.
-    try:
-        with open('inventory.txt', 'r') as file:
-            totalorderlist = file.readlines()
-            print("Current Orders:")
-            print(totalorderlist)
-    #If the inventory file does not exist, start with an empty inventory and continue running without producing an error.
-    except FileNotFoundError:
-         totalorderlist = []
-    return totalorderlist
-
-def backup(totalorderlist,neworderlist):
-    totalorderlist.extend(neworderlist)
-    with open('inventory.txt', 'a') as file:
-        print("New orders Added:")
-        #Print each product [Order no, Product Name, Quantity]:
-        for o in range(0, len(neworderlist), 3):
-            #file.write(neworderlist[o : o + 3] + '\n')
-            print(neworderlist[o : o + 3] + '\n')
-    print("Order sucessfully saved to inventory.txt")
-    return totalorderlist
-
-
-#2. History Tracking: Use a Python list (array) to store every valid transaction 
-#amount entered. 
-neworderlist = []
-
-#1. Initialize the inventory to zero in the start
 quit = False
-entrycount = 0
 current_total = 0
 failed_attempts = 0
 totaltax = 0
+final_total = 0
+entry_count = 0
+#2. History Tracking: Use a Python list (array) to store every valid transaction 
+#amount entered. 
+totalOrderList = []
+newOrderList = []
+recentInput = []
 
-#4a. get_valid_input(): Handles the prompt, handles input validation, and returns a valid integer or a "quit" signal. 
-def get_valid_input():
+
+#4. Modularity: Maintain your functional design. Create a load_inventory() and save_inventory() function. 
+def load_inventory(totalOrderList,entry_count, final_total):
+    #Persistence: At the start of the program, read the information previously saved in the inventory file.
+    try:
+        with open('inventory.txt', 'r') as file:
+            totalOrderList = file.readlines()
+            print(f"Current Orders: {totalOrderList}")
+            print(f"Overall stock input: {final_total}")
+            entry_count = int(totalOrderList[-3]) if totalOrderList else 0
+    #If the inventory file does not exist, start with an empty inventory and continue running without producing an error.
+    except FileNotFoundError:
+         totalOrderList = []
+         entry_count = 0
+         final_total = 0
+    return totalOrderList, entry_count, final_total
+
+def save_inventory(totalOrderList,newOrderList):
+    totalOrderList.extend(newOrderList)
+    with open('inventory.txt', 'a') as file:
+        print("New orders Added:")
+        #Print each product [Order no, Product Name, Quantity]:
+        for o in range(0, len(newOrderList), 3):
+            chunk = newOrderList[o : o + 3]
+            newTxtLine = ', '.join(str(x) for x in chunk)
+            file.write(newTxtLine + '\n')
+            print(newTxtLine)
+    file.close()
+    print("Order sucessfully saved to inventory.txt")
+    return totalOrderList, newOrderList
+
+#Handles the prompt, handles input validation, and returns a valid integer or a "quit" signal. 
+def get_valid_input(entry_count):
     new_value = input("Enter stock quantity (or type 'quit' to exit): ")
+    product_name = input("Enter product name: ") 
     if new_value.lower() == 'quit':  
-            return 'quit', False
+            return 'quit', False, entry_count
     if not (new_value.lstrip('-').isdigit()):
             print("Error: pls enter a valid number or type 'quit' to exit.")
-            return None, True
+            return None, True, entry_count
     new_value = int(new_value)
     if new_value < 0:
             print("Error: Negative numbers are not allowed. Pls input a positive number or type 'quit' to exit.")
-            return None, True
+            return None, True, entry_count
     else:
-        return new_value, False
+        #Update any counters and records you are tracking, such as the number of deliveries processed. 
+        entry_count += 1
+        recentInput = [entry_count, product_name, new_value]
+        print(f"\n\033[1m Entry Count {entry_count} , {product_name}\033[0m")
+        #newOrderList.append(recentInput)
+        return recentInput, False, entry_count, product_name, new_value
 
- #4b. process_delivery(current_total, new_value): Calculates the new total and returns it.        
+ #Calculates the new total and returns it.        
 def process_delivery(current_total, new_value):
     current_total += new_value
     print("Stock Quantity entered:", new_value)
-    print("Overall Stock Quantity :", current_total)
     return current_total, new_value
 
-#4c. calculate_tax(amount): A new requirement! This function takes a delivery amount and returns the tax (10% of that specific delivery). 
+#Takes a delivery amount and returns the tax (10% of that specific delivery). 
 def calculate_tax(new_value):
     tax = new_value * 0.10
     print("Tax for this delivery: S$", tax)
     return tax
 
-#4d. generate_report(current_total, entrycount, totaltax, failed_attempts): A dedicated function to print the final summary. 
-def generate_report(current_total, entrycount,totaltax, failed_attempts):
+#Print the final summary. 
+def generate_report(current_total, totaltax, failed_attempts):
     print("\n\033[1m", "Report Summary:", "\033[0m")
     print("Total Units Processed:", current_total)
-    print("Total Deliveries Processed:", entrycount)
     print("Total Tax Paid: S$", totaltax)
     print("Number of Failed/Rejected Entries:", failed_attempts)
 
 def overstock_alert(current_total):
     if current_total > 500:
-        print("ALERT: Overstock! Total inventory exceeds 500 units!")
+        print("ALERT: Overstock! Total inventory input exceeds 500 units!")
         return True
     return False
 
-load_inventory()
-#2. Run in a continuous loop asking user to enter a stock quantity, until the user types quit. 
+totalOrderList, entry_count, final_total = load_inventory(totalOrderList, entry_count, final_total)
+#Continuous loop asking user to enter a stock quantity, until the user types quit. 
 while quit == False:
-    new_value, has_error = get_valid_input()
+    recentInput = []
+    recentInput, has_error, entry_count = get_valid_input(recentInput, entry_count)
     if new_value == 'quit':
         quit = True
         break
@@ -90,20 +103,18 @@ while quit == False:
         failed_attempts += 1
         print("Number of Failed/Rejected Entries:", failed_attempts)
         continue
-    #3. If the user enters a valid value:  
-    #3c. Update any counters and records you are tracking, such as the number of deliveries processed. 
-    entrycount += 1
-    print("\n\033[1m", "Entry Count:", entrycount, "\033[0m")
-    #3a. Add the delivery amount to the running total. 
+    #Add the delivery amount to the running total. 
+    print(recentInput)
+    new_value = recentInput[2]
+    newOrderList.extend(recentInput)
     current_total, new_value = process_delivery(current_total, new_value)
-    #3b. Calculate the tax for that delivery. 
+    #Calculate the tax for that delivery. 
     tax = calculate_tax(new_value)
-    #3c. Update any counters and records you are tracking, such as the number of deliveries processed. 
+    #Update any counters and records you are tracking, such as the number of deliveries processed. 
     totaltax += tax
     quit = overstock_alert(current_total)
+generate_report(current_total, totaltax, failed_attempts)
 
 #3. Write-Back: When the user types quit, save the final total and the transaction history list to inventory.txt. 
+save_inventory(totalOrderList,newOrderList)
 
-
-generate_report(current_total, entrycount, totaltax, failed_attempts)
-file.close()  
